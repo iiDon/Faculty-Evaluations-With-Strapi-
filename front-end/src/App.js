c29f6e73c64aa0e40e7d0c25ea02ff511af4df20
Sultan <abu-farris@hotmail.com>
2022-06-30 16:11:08 +0300

    initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+
+  console.log("first")
+  return (
+    <div className="App">
+      أهلاcodkslpffkdffdsffsd;sdkofsllll بالعالgم
+    </div>
+  );
+}
+
+export default App;
